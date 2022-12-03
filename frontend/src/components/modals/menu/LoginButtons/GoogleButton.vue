f407361847790dac3468fcd57713c98eb6bd54c4
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2022-12-03 19:11:55 +0100

    fix(frontend): Apply Prettier format rules

--- frontend/src/components/modals/menu/LoginButtons/GoogleButton.vue
+++ frontend/src/components/modals/menu/LoginButtons/GoogleButton.vue
@@ -1,37 +1,35 @@
 <template>
-  <div class="google-login-button">
-    <img class="google-login-button__logo" src="./google-logo.svg" alt="Google logo">
-    <span class="google-login-button__text">Zaloguj z Google</span>
-  </div>
+    <div class="google-login-button">
+        <img class="google-login-button__logo" src="./google-logo.svg" alt="Google logo" />
+        <span class="google-login-button__text">Zaloguj z Google</span>
+    </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style>
-div.google-login-button{
-  display: flex;
-  align-items: center;
-  padding: 0 0.5rem;
+div.google-login-button {
+    display: flex;
+    align-items: center;
+    padding: 0 0.5rem;
 
-  width: 100%;
-  height: 45px;
-  box-shadow: 0 0 6px 3px rgba(213, 213, 213, 0.73);
+    width: 100%;
+    height: 45px;
+    box-shadow: 0 0 6px 3px rgba(213, 213, 213, 0.73);
 
-  cursor: pointer;
-  user-select: none;
+    cursor: pointer;
+    user-select: none;
 }
 
-div.google-login-button:hover  {
-  background-color: #f4f4f4;
+div.google-login-button:hover {
+    background-color: #f4f4f4;
 }
 
-img.google-login-button__logo{
-  margin-right: 1.5rem;
+img.google-login-button__logo {
+    margin-right: 1.5rem;
 }
 
-span.google-login-button__text{
-  font-size: medium;
+span.google-login-button__text {
+    font-size: medium;
 }
 </style>
--- frontend/src/components/modals/menu/LoginModal.vue
+++ frontend/src/components/modals/menu/LoginModal.vue
@@ -1,72 +1,75 @@
 <template>
-  <AlgoModal class="login_modal" title="Zaloguj się">
-    <form class="login_modal__form" @submit.prevent="login()">
-      <label for="username-input">Nazwa użytkownika</label>
-      <input type="text" v-model="username" id="username-input" class="full-size" placeholder="Nazwa użytkownika"/>
-      <label for="password-input">Hasło</label>
-      <input type="password" v-model="password" id="password-input" class="full-size" placeholder="Hasło"/>
-    </form>
+    <AlgoModal class="login_modal" title="Zaloguj się">
+        <form class="login_modal__form" @submit.prevent="login()">
+            <label for="username-input">Nazwa użytkownika</label>
+            <input
+                type="text"
+                v-model="username"
+                id="username-input"
+                class="full-size"
+                placeholder="Nazwa użytkownika"
+            />
+            <label for="password-input">Hasło</label>
+            <input type="password" v-model="password" id="password-input" class="full-size" placeholder="Hasło" />
+        </form>
 
-    <hr/>
+        <hr />
 
-    <div class="login_modal__third_party_services">
-      <GoogleButton @click="loginWithGoogle()"/>
-    </div>
+        <div class="login_modal__third_party_services">
+            <GoogleButton @click="loginWithGoogle()" />
+        </div>
 
-    <template #buttons>
-      <AlgoButton type="ok" @click="login()">Zaloguj się</AlgoButton>
-    </template>
-  </AlgoModal>
+        <template #buttons>
+            <AlgoButton type="ok" @click="login()">Zaloguj się</AlgoButton>
+        </template>
+    </AlgoModal>
 </template>
 
 <script>
 import AlgoModal from "@/components/global/AlgoModal.vue";
 import AlgoButton from "@/components/global/AlgoButton.vue";
-import GoogleButton from "@/components/modals/menu/LoginButtons/GoogleButton.vue"
+import GoogleButton from "@/components/modals/menu/LoginButtons/GoogleButton.vue";
 
 export default {
-  data() {
-    return {
-      username: "",
-      password: ""
-    }
-  },
-
-  methods: {
-    login() {
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
     },
 
-    loginWithGoogle() {
+    methods: {
+        login() {},
 
-    }
-  },
+        loginWithGoogle() {},
+    },
 
-  components: {GoogleButton, AlgoButton, AlgoModal},
+    components: { GoogleButton, AlgoButton, AlgoModal },
 };
 </script>
 
 <style scoped>
 .dialog {
-  width: 25rem;
+    width: 25rem;
 }
 
 .dialog-content {
-  overflow: visible !important;
+    overflow: visible !important;
 }
 
 hr {
-  margin-bottom: 1rem;
-  border-color: #f1f1f1;
+    margin-bottom: 1rem;
+    border-color: #f1f1f1;
 }
 
-input[type="text"], input[type="password"] {
-  height: 2rem;
-  margin-bottom: 1rem;
+input[type="text"],
+input[type="password"] {
+    height: 2rem;
+    margin-bottom: 1rem;
 }
 
 div.login_modal__third_party_services {
-  margin: 0 0.2rem;
-  padding-bottom: 1rem;
+    margin: 0 0.2rem;
+    padding-bottom: 1rem;
 }
-
 </style>
